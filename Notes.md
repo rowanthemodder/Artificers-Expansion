e8a10f4d8a74fa71df93522c819763dfd602fe99
rowanthemodder <46363164+rowanthemodder@users.noreply.github.com>
2022-05-27 05:02:51 -0700

    Hopefully Everything

---
+++ Notes.md
@@ -0,0 +1,24 @@
+
+Animus Brass
+
+Arcane Hand
+
+Vressa's Arcane Hand
+
+Greater Arcane Reach
+
+Galvanised Argent
+
+Forged Zinc
+
+Lesser Cogwheel
+
+Greater Cogwheel
+
+Infused Silk Belt
+
+Animus Tube
+
+Marrowwood (Brown)
+
+Heartwood (Purple)
---
+++ src/main/java/com/rowanthemodder/artificersexpansion/ArtificersExpansionMod.java
@@ -0,0 +1,38 @@
+package com.rowanthemodder.artificersexpansion;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("artificersexpansion")
+public class ArtificersExpansionMod
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ArtificersExpansionMod() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        // Register the processIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
