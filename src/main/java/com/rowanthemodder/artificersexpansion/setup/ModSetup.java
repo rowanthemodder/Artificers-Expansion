487bc809f20e9a0876167c1f4d0c3e257b2d853f
rowanthemodder <Atomically@protonmail.com>
2023-04-18 02:39:44 -0700

    Setting up mod

--- src/main/java/com/rowanthemodder/artificersexpansion/setup/ModSetup.java
+++ src/main/java/com/rowanthemodder/artificersexpansion/setup/ModSetup.java
@@ -1,9 +1,20 @@
 package com.rowanthemodder.artificersexpansion.setup;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class ModSetup {
-    
+
+    public static final String TAB_NAME = "artificersexpansion";
+
+    public static final CreativeModeTab ITEM_GROUP = new CreativeModeTab(TAB_NAME) {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(Registration.HEARTWOOD_PLANKS_ITEM.get());
+        }
+    };
+
     public static void init(FMLCommonSetupEvent event) {
 
     }
--- src/main/java/com/rowanthemodder/artificersexpansion/setup/Registration.java
+++ src/main/java/com/rowanthemodder/artificersexpansion/setup/Registration.java
@@ -1,8 +1,35 @@
 package com.rowanthemodder.artificersexpansion.setup;
 
-public class Registration {
-    
-    public static void init() {
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
+import static com.rowanthemodder.artificersexpansion.ArtificersExpansion.MODID;
+
+public class Registration {
+
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static void init() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        BLOCKS.register(bus);
+        ITEMS.register(bus);
+    }
+
+    public static final BlockBehaviour.Properties PLANKS_PROPERTIES = BlockBehaviour.Properties.of(Material.NETHER_WOOD).strength(2f);
+    public static final Item.Properties ITEM_PROPERTIES = new Item.Properties().tab(ModSetup.ITEM_GROUP);
+
+    public static final RegistryObject<Block> HEARTWOOD_PLANKS = BLOCKS.register("heartwood_planks", () -> new Block(PLANKS_PROPERTIES));
+    public static final RegistryObject<Item> HEARTWOOD_PLANKS_ITEM = fromBlock(HEARTWOOD_PLANKS);
+
+    public static <B extends Block> RegistryObject<Item> fromBlock(RegistryObject<B> block){
+        return ITEMS.register(block.getId().getPath(), () -> new BlockItem(block.get(), ITEM_PROPERTIES));
     }
 }
